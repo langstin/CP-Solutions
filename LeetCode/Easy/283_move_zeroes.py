a73cb3e26bdf6f9146fd1f185e6a80806e13c422
Mhd-Nafil-A-K <mhdnafil001@gmail.com>
2026-06-24 16:52:11 +0530

    Implement moveZeroes method to shift zeros

---
+++ LeetCode/Easy/283_move_zeroes.py
@@ -0,0 +1,7 @@
+class Solution:
+    def moveZeroes(self, nums) -> None:
+        l = 0
+        for i in range(len(nums)):
+          if nums[i] != 0:
+            nums[l],nums[i]=nums[i],nums[l]
+            l+= 1
