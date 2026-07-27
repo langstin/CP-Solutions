7bcba3b71c4bc0828fde1564e8e8ac51f94cc493
langstin <mhdnafil001@gmail.com>
2026-07-27 09:14:01 +0530

    Add Search Insert Position solution (Binary Search, O(log n))

---
+++ LeetCode/Easy/35_search_insert_position.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchInsert(self, nums,t):
+      l=0
+      r=len(nums)-1     
+      while l<=r:
+        mid=(l+r)//2
+        if nums[mid]==t:
+          return mid
+        elif nums[mid]>t:
+          r=mid-1
+        else:
+          l=mid+1
+      return l
+
